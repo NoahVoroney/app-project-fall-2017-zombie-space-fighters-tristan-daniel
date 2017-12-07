15c7702e9a6e37ea00b5e4e27396a96eb766566c
Tristan Kalabric <tristan.kalabric@ocsbstudent.ca>
2017-12-07 10:39:17 -0500

    removed global variables

--- level1_question.lua
+++ level1_question.lua
@@ -121,7 +121,7 @@ end
 
 
 --adding the event listeners 
-local function AddTextListeners ( )
+local function AddTextListeners ()
     answerText:addEventListener( "touch", TouchListenerAnswer )
     wrongText1:addEventListener( "touch", TouchListenerWrongAnswer)
     wrongText2:addEventListener( "touch", TouchListenerWrongAnswer2)
