1b195c47f89cc0fe981d93c4e1b962a15d63faa1
Ms Raffin <Lynnette.Raffin@ocsb.ca>
2017-12-14 08:37:39 -0500

    Updated icon files

--- config.lua
+++ config.lua
@@ -1,29 +1,23 @@
-application = {
-	content = {
+--
+-- For more information on config.lua see the Project Configuration Guide at:
+-- https://docs.coronalabs.com/guide/basics/configSettings
+--
+
+application =
+{
+	content =
+	{
 		width = 768,
 		height = 1024, 
-		scale = "letterBox",
-		fps = 30,
+		scale = "letterbox",
+		fps = 60,
 		
 		--[[
-        imageSuffix = {
-		    ["@2x"] = 2,
-		}
+		imageSuffix =
+		{
+			    ["@2x"] = 2,
+			    ["@4x"] = 4,
+		},
 		--]]
 	},
-
-    --[[
-    -- Push notifications
-
-    notification =
-    {
-        iphone =
-        {
-            types =
-            {
-                "badge", "sound", "alert", "newsstand"
-            }
-        }
-    }
-    --]]    
 }
